83ead18adbfe7ac7d4b696fc9d3683a9316cb721
ginsama01 <ginsama2001@gmail.com>
2022-05-11 13:43:35 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:http/http.dart' as http;
@@ -29,85 +31,190 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List data;
+  final String baseUrl = 'api.openweathermap.org';
+  final String API_KEY = "e45ecdc3bb276b3b0525f9b3d20a9d4a";
 
-  Future<String> getData() async {
-    var response = await http.get(
-        Uri.encodeFull("https://61af70223e2aba0017c49342.mockapi.io/weather"),
-        headers: {"Accept": "application/json"});
+  Icon customIcon = const Icon(Icons.search);
+  Widget customSearchBar = const Text('Temperature info');
+  Map<String, dynamic> data;
 
-    setState(() {
-      data = json.decode(response.body);
-    });
-    return "Success";
+  Future<void> getData(String city) async {
+    Map<String, String> parametes = {'q': city, 'appid': API_KEY};
+
+    Uri uri = Uri.https(
+      baseUrl,
+      '/data/2.5/weather',
+      parametes,
+    );
+    print(uri);
+    Map<String, String> headers = {
+      HttpHeaders.contentTypeHeader: 'application/json'
+    };
+
+    var response = await http.get(uri, headers: headers);
+
+    if (response.statusCode == 200) {
+      print(json.decode(response.body)['weather'][0]['icon']);
+      setState(() {
+        data = json.decode(response.body);
+      });
+    } else {
+      setState(() {
+        data = {"error": "Can't search this location"};
+      });
+      throw json.decode(response.body)['error']['message'];
+    }
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getData('Hanoi');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Flutter Weather Api Example"),
-      ),
-      body: Center(
-        child: getList(),
-      ),
-    );
-  }
-
-  Widget getList() {
-    if (data == null || data.length < 1) {
-      return Container(
-        child: Center(
-          child: Text("Please wait..."),
+        appBar: AppBar(
+          title: customSearchBar,
+          automaticallyImplyLeading: false,
+          actions: [
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    if (customIcon.icon == Icons.search) {
+                      customIcon = const Icon(Icons.cancel);
+                      customSearchBar = ListTile(
+                        leading: Icon(
+                          Icons.search,
+                          color: Colors.white,
+                          size: 28,
+                        ),
+                        title: TextField(
+                          decoration: InputDecoration(
+                            hintText: 'Type in city name...',
+                            hintStyle: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontStyle: FontStyle.italic,
+                            ),
+                            border: InputBorder.none,
+                          ),
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                          onSubmitted: (String value) {
+                            getData(value);
+                          },
+                        ),
+                      );
+                    } else {
+                      customIcon = const Icon(Icons.search);
+                      customSearchBar = const Text('Temperature info');
+                    }
+                  });
+                },
+                icon: customIcon)
+          ],
         ),
-      );
-    }
-    return ListView.separated(
-      itemCount: data?.length,
-      itemBuilder: (BuildContext context, int index) {
-        return getListItem(index);
-      },
-      separatorBuilder: (context, index) {
-        return Divider();
-      },
-    );
-  }
-
-  Widget getListItem(int i) {
-    if (data == null || data.length < 1) return null;
-    if (i == 0) {
-      return Container(
-        margin: EdgeInsets.all(4),
-        child: Center(
-          child: Text(
-            "Locations",
-            style: TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-      );
-    }
-
-    return Container(
-      child: Container(
-        margin: EdgeInsets.all(4.0),
-        child: Padding(
-          padding: EdgeInsets.all(4),
-          child: Text(
-          "Location: " + data[i]['location'].toString() + ' - Temperature: ' + data[i]['temperature'].toString(),
-            style: TextStyle(fontSize: 18),
-          ),
-          
-        ),
-      ),
-    );
+        body: data != null
+            ? (data['weather'] != null
+                ? Center(
+                    child: Column(children: [
+                      Image.network("http://openweathermap.org/img/w/" +
+                          data['weather'][0]['icon'] +
+                          ".png"),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Text(
+                        "${(data['main']['temp'] - 273.15).toStringAsFixed(1)}°",
+                        style: TextStyle(
+                          fontSize: 46.0,
+                        ),
+                      ),
+                      SizedBox(height: 10.0),
+                      Text(
+                        "${data['name']}",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                        ),
+                      ),
+                      SizedBox(height: 10.0),
+                      Divider(),
+                      Container(
+                        margin: EdgeInsets.all(20.0),
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "Wind",
+                                  ),
+                                  SizedBox(height: 18.0),
+                                  Text(
+                                    "Pressure",
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "${data['wind']['speed']}",
+                                  ),
+                                  SizedBox(height: 18.0),
+                                  Text(
+                                    "${data['main']['pressure']}",
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "Humidity",
+                                  ),
+                                  SizedBox(height: 18.0),
+                                  Text(
+                                    "Feels Like",
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "${data['main']['humidity']}",
+                                  ),
+                                  SizedBox(height: 18.0),
+                                  Text(
+                                    "${(data['main']['feels_like'] - 273.15).toStringAsFixed(1)}",
+                                  ),
+                                ],
+                              ),
+                            ]),
+                      )
+                    ]),
+                  )
+                : Center(
+                    child: Text(
+                      "${data['error']}",
+                    ),
+                  ))
+            : Center(
+                child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(
+                    Theme.of(context).primaryColor, // Red
+                  ),
+                ),
+              ));
   }
 }
